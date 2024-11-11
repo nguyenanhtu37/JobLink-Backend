84807e4d30a0d057b0de0fdaf172dc4344fae695
Nguyen Anh Tu <nguyenanhtu3703@gmail.com>
2024-11-11 23:41:37 +0700

    Commit lan 3

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const adminRouter = require("./routes/adminRouter");
 const app = express();
 const port = process.env.PORT || 8888;
 
-app.use(cors());
+app.use(cors({ origin: 'https://your-vercel-domain.vercel.app' }));
 app.use(cookie_parser());
 app.use(express.json({ limit: '100mb' })); 
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
